fb956192cd00e3c0a4457d4562aeca6dca3ce65b
deepak sharma <deepaksharma77899@gmail.com>
2023-12-29 14:36:09 +0530

    models added and created folder structure

---
+++ backend/models/bankdetails.js
@@ -0,0 +1,36 @@
+module.exports = (sequelize, dataTypes) => {
+
+    const bankdetails = sequelize.define('bankdetails', {
+        rollNo: {
+            type: dataTypes.INTEGER,
+            primaryKey:true,
+        },
+        accHolderName: {
+            type: dataTypes.STRING,
+        },
+        bankName: {
+            type: dataTypes.STRING,
+        },
+        accNumber: {
+            type: dataTypes.INTEGER,
+        },
+        IFSC: {
+            type:dataTypes.STRING,
+        },
+        lastUpdatedBy:{
+           type:dataTypes.STRING,
+           allowNull: false,
+           field:'last_updated_by'
+        },
+    },{
+        updatedAt: 'last_updated_at'
+    })
+    bankdetails.associate = (models) => {
+        bankdetails.belongsTo(models.students, {
+            foreignKey: {
+                name: 'rollNo'
+              }
+          });
+      };
+    return bankdetails;
+}
---
+++ backend/models/rooms.js
@@ -0,0 +1,49 @@
+module.exports = (sequelize, dataTypes) => {
+
+    const rooms = sequelize.define('rooms', {
+        roomId: {
+            type: dataTypes.INTEGER,
+            primaryKey:true,
+        },
+        roomNo: {
+            type: dataTypes.INTEGER,
+        },
+        block: {
+            type: dataTypes.STRING,
+        },
+        floorNo: {
+            type: dataTypes.INTEGER,
+        },
+        currentOccupancy: {
+            type: dataTypes.INTEGER,
+        },
+        maxOccupancy: {
+            type: dataTypes.INTEGER,
+        },
+        lastUpdatedBy:{
+           type:dataTypes.STRING,
+           allowNull: false,
+           field:'last_updated_by'
+        },
+    },{
+        updatedAt: 'last_updated_at'
+    })
+    rooms.associate = (models) => {
+        rooms.hasMany(models.students, {
+            foreignKey: {
+                name: 'roomId'
+              }
+          });
+        rooms.belongsTo(models.hostels, {
+            foreignKey: {
+                name: 'hostelNo'
+              }
+          });
+        rooms.belongsTo(models.roomtypes, {
+            foreignKey: {
+                name: 'roomTypeNo'
+              }
+          });
+      };
+    return rooms;
+}
