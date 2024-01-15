dfca12a37bb4a67b2c0868636dff9144d8df102b
deepak sharma <deepaksharma77899@gmail.com>
2024-01-16 00:19:26 +0530

    update bulk api added

--- backend/routers/hostelAuthority/routes.js
+++ backend/routers/hostelAuthority/routes.js
@@ -9,6 +9,7 @@ const { deleteStudent } = require('../../controllers/hostelAuthority/studentModu
 const { singleStudentInfo } = require('../../controllers/hostelAuthority/studentModule/singleStudentInfo');
 const { singleStudentUpload } = require('../../controllers/hostelAuthority/studentModule/singleStudentUpload');
 const { downloadFile } = require('../../controllers/hostelAuthority/studentModule/downloadFile');
+const { updateBulk } = require('../../controllers/hostelAuthority/studentModule/updateBulk');
 const AdminLogin = require('../../controllers/Login/AdminLogin')
 const AdminLogout = require('../../controllers/LoggingOut/AdminLogOut');
 const isCookie = require('../../controllers/isCookie');
@@ -30,6 +31,7 @@ router.get('/', (req, res) => {
     return res.send('success')
 })
 router.post('/bulkCreate', upload.single('file'), csvToJsonConverter, bulkCreateController);
+router.patch('/updateBulk',upload.single('file'), csvToJsonConverter, updateBulk);
 router.get('/studentsInfo', studentsInfo);
 router.post('/singleStudentUpload', singleStudentUpload);
 router.get('/student/:rollNo', singleStudentInfo);
