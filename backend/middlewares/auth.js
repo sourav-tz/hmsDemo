89dd30e83f9b55979578dc68ce8411bb7dbba76b
Aditya kumar <79767847+Addiii111@users.noreply.github.com>
2024-01-21 11:48:50 +0530

    Complete Authorization middleware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,19 +1,35 @@
-
+const jwt = require('jsonwebtoken')
+require("dotenv").config();
 
 const auth = (req, res, next) => {
 
     try {
-        const token = req.headers.cookie
 
-        if (!token) {
-            return res.status(401).json({ message: 'Token is expired you can logout' })
+        const cookies = req.headers.cookie;
 
+        if (!cookies) {
+            return res.status(401).send('cookie expired in auth');
         }
 
+        const token = cookies.split("=")[1];
 
+        if (!token) {
+            return res.status(401).send('Token is missing');
+        }
 
+        try {
+            const verifyUser = jwt.verify(
+                token,
+                process.env.JWT_SECRET_KEY
+            )
+            console.log("verify user in auth middleware = " + verifyUser);
+        } catch (error) {
+            return res.status(401).send("invalid token")
+        }
+
+        next();
     } catch (error) {
-        res.status(401).json({ message: error.message });
+        return res.status(401).send("Unauthorized request");
     }
 
 }
