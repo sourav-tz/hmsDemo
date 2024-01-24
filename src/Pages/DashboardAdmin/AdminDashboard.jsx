3ec4c5e0d0e8920f74851a720bcb078a178c5d8a
deepak sharma <deepaksharma77899@gmail.com>
2024-01-24 17:10:15 +0530

    courses and hostel backend apis

--- src/Pages/DashboardAdmin/AdminDashboard.jsx
+++ src/Pages/DashboardAdmin/AdminDashboard.jsx
@@ -14,8 +14,6 @@ const StudentUploadInfo = lazy(()=>import('./StudentsInfo/UploadInfo/UploadInfo'
 const StudentViewInfo = lazy(()=>import('./StudentsInfo/ViewInfo/ViewInfo'));
 const RoomsAllotement = lazy(()=>import('./RoomInfo/AllotRooms/AllotRooms'));
 const RoomsUpload = lazy(()=>import('./RoomInfo/UploadInfo/RoomsUpload'));
-const Hostels = lazy(()=>import('./SuperAdmin/Hostels/Hostels'));
-const Courses = lazy(()=>import('./SuperAdmin/Courses/Courses'));
 import { driver } from "driver.js";
 import "driver.js/dist/driver.css";
 
@@ -120,21 +118,6 @@ return <>
         <div className={styles.contentSpace}>
             <Suspense fallback={<Loadingpage />}>
                 <RoomsUpload />
-            </Suspense>
-        </div>
-        :null}
-        
-        {activeOptions==='saHostels'?
-        <div className={styles.contentSpace}>
-            <Suspense fallback={<Loadingpage />}>
-                <Hostels />
-            </Suspense>
-        </div>
-        :null}
-        {activeOptions==='saCourses'?
-        <div className={styles.contentSpace}>
-            <Suspense fallback={<Loadingpage />}>
-                <Courses />
             </Suspense>
         </div>
         :null}
