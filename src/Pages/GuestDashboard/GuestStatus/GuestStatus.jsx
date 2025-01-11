08eed816b30bf22c4c4c4c9da8a736ab7ffef699
TwoLobster <rahulkaundal31000@gmail.com>
2025-01-11 23:42:43 +0530

    minor changes

--- src/Pages/GuestDashboard/GuestStatus/GuestStatus.jsx
+++ src/Pages/GuestDashboard/GuestStatus/GuestStatus.jsx
@@ -76,7 +76,7 @@ const GuestStatus = () => {
       <div className="flex flex-col min-h-screen items-center py-10">
           <h1 className="text-3xl font-semibold mb-10">Check Application Status</h1>
         <div className="z-10 flex flex-col items-center">
-          <div className="bg-[#5757FF] rounded-lg p-10 w-[400px] sm:w-[450px] text-white shadow-lg">
+          <div className="bg-[#5757FF] rounded-2xl p-10 w-[400px] sm:w-[450px] text-white shadow-lg">
             <div className="mb-6">
               <label htmlFor="application-id" className="block text-lg font-medium mb-3">
                 Enter Application ID
