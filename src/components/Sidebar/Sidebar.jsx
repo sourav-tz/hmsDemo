285816c59929aa72fd82f34143d85710987f3218
Rahul Vashishtha <68159064+RahulVashishtha1@users.noreply.github.com>
2025-05-01 23:00:34 +0530

    fix dev admin sidebar (#257)

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,7 +13,11 @@ import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { FaNoteSticky } from "react-icons/fa6";
 import { SlSupport } from "react-icons/sl";
+import { FaRegFileAlt } from 'react-icons/fa';
 import { removeUserData } from '../../Store/Reducers/userSlice';
+
+
+
 
 
 
@@ -42,6 +46,8 @@ export default function Sidebar(){
     const [subSettings, setSubSettings] = useState(false);
     const [subNotice, setSubNotice] = useState(false);
     const [subComplaint,setSubComplaint] = useState(false);
+    const [subApplication,setsubApplication] = useState(false);
+    const [subApplicationStatus,setsubApplicationStatus] = useState(false);
     const [subGuest,setSubGuest] = useState(false);
 
     const openMenu = ()=>{
@@ -61,6 +67,9 @@ export default function Sidebar(){
             setSubSettings(false);
             setSubNotice(false);
             setSubComplaint(false);
+            setsubApplication(false);
+            setsubApplicationStatus(false);
+            setsubApplicationStatus(false);
             setSubGuest(false);
         }else if(value ==='studentInfo'){
             setSubHome(false);
@@ -69,6 +78,9 @@ export default function Sidebar(){
             setSubSettings(false);
             setSubNotice(false);
             setSubComplaint(false);
+            setsubApplication(false);
+            setsubApplicationStatus(false);
+            setsubApplicationStatus(false);
             setSubGuest(false);
         }else if(value === 'roomInfo'){
             setSubHome(false);
@@ -77,6 +89,9 @@ export default function Sidebar(){
             setSubSettings(false);
             setSubNotice(false);
             setSubComplaint(false);
+            setsubApplication(false);
+            setsubApplicationStatus(false);
+            setsubApplicationStatus(false);
             setSubGuest(false);
         }else if(value === 'settings'){
             setSubHome(false);
@@ -85,6 +100,9 @@ export default function Sidebar(){
             setSubSettings(prev=>!prev);
             setSubNotice(false);
             setSubComplaint(false);
+            setsubApplication(false);
+            setsubApplicationStatus(false);
+            setsubApplicationStatus(false);
             setSubGuest(false);
         }else if(value === 'notice'){
             setSubHome(false);
@@ -93,6 +111,9 @@ export default function Sidebar(){
             setSubSettings(false);
             setSubNotice(prev=>!prev);
             setSubComplaint(false);
+            setsubApplication(false);
+            setsubApplicationStatus(false);
+            setsubApplicationStatus(false);
             setSubGuest(false);
         }else if(value === 'complaint'){
             setSubHome(false);
@@ -101,7 +122,45 @@ export default function Sidebar(){
             setSubSettings(false);
             setSubNotice(false);
             setSubComplaint(prev => !prev);
+            setsubApplication(false);
+            setsubApplicationStatus(false);
+            setsubApplicationStatus(false);
             setSubGuest(false);
+        }
+        else if(value === 'application'){
+            setSubHome(false);
+            setSubStudent(false);
+            setSubRoom(false);
+            setSubSettings(false);
+            setSubNotice(false);
+            setSubComplaint(false);
+            setsubApplication(prev => !prev);
+            setsubApplicationStatus(false);
+            setSubGuest(false);
+
+        }else if(value === 'applicationstatus'){
+            setSubHome(false);
+            setSubStudent(false);
+            setSubRoom(false);
+            setSubSettings(false);
+            setSubNotice(false);
+            setSubComplaint(false);
+            setsubApplication(false);
+            setsubApplicationStatus(prev => !prev);
+            setsubApplicationStatus(false);
+            setSubGuest(false);
+
+        }else if(value === 'applicationstatus'){
+            setSubHome(false);
+            setSubStudent(false);
+            setSubRoom(false);
+            setSubSettings(false);
+            setSubNotice(false);
+            setSubComplaint(false);
+            setsubApplication(false);
+            setsubApplicationStatus(prev => !prev);
+            setSubGuest(false);
+
         }
         else if(value == 'guest'){
             setSubHome(false);
@@ -110,6 +169,9 @@ export default function Sidebar(){
             setSubSettings(false);
             setSubNotice(false);
             setSubComplaint(false);
+            setsubApplication(false);
+            setsubApplicationStatus(false);
+            setsubApplicationStatus(false);
             setSubGuest(prev => !prev);
         }
 
@@ -197,6 +259,8 @@ export default function Sidebar(){
                         <li onClick={()=>{navigator('/adminDashboard/studentInfo/uploadInfo')}} className={styles.subOptions+' ' + (activeSubOption==='uploadInfo'?styles.activeSubOption:null)}>Upload Info</li>
                         <li onClick={()=>{navigator('/adminDashboard/studentInfo/register')}} className={styles.subOptions+' ' + (activeSubOption==='register'?styles.activeSubOption:null)}>Register Student</li>
                         </ul>
+
+                        {/* heere */}
             </div>
             <div  className={styles.item +' '+(state?styles.ItemOpenMenu:styles.ItemCloseMenu)}>
                         <p onClick={()=>{changeSubMenu('roomInfo')}} className={(activeOption==='roomInfo'?styles.activeItem:null)+ ' flex items-center gap-2'}><MdOutlineBedroomChild /> <span className={(state?null:styles.hidden)+' mt-1'}>Room Info</span></p>
@@ -204,6 +268,8 @@ export default function Sidebar(){
                         <li onClick={()=>{navigator('/adminDashboard/roomInfo/allotRooms')}} className={(state?null:styles.hidden)+' '+styles.subOptions+' ' + (activeSubOption==='allotRooms'?styles.activeSubOption:null)}>Allot Rooms</li>
                         </ul>
             </div>
+
+            {/* notice */}
             <div  className={styles.item +' '+(state?styles.ItemOpenMenu:styles.ItemCloseMenu)}>
                         <p onClick={()=>{changeSubMenu('notice')}} className={(activeOption==='notice'?styles.activeItem:null)+ ' flex items-center gap-2'}><FaNoteSticky /> <span className={(state?null:styles.hidden)+' mt-1'}>Notice</span></p>
                         <ul className={state&&subNotice?null:styles.hidden}>
@@ -211,13 +277,23 @@ export default function Sidebar(){
                         <li onClick={()=>{navigator('/adminDashboard/notice/viewNotice')}} className={(state?null:styles.hidden)+' '+styles.subOptions+' ' + (activeSubOption==='viewNotice'?styles.activeSubOption:null)}>View Notice</li>
                         </ul>
             </div>
+            
+            {/* Complaints */}
             <div  className={styles.item +' '+(state?styles.ItemOpenMenu:styles.ItemCloseMenu)}>
-                        <p onClick={()=>{changeSubMenu('complaint');Navigator('/adminDashboard/complaints/complaints')}} className={(activeOption==='complaints'?styles.activeItem:null)+ ' flex items-center gap-2'}><SlSupport /> <span className={(state?null:styles.hidden)+' mt-1'}>Complaint</span></p>
-                        {/* <ul className={state&&subStudent?null:styles.hidden}> */}
-                        {/* <li onClick={()=>{Navigator('/superAdminDashboard/studentActions/viewInfo')}} className={styles.subOptions+' ' + (activeSubOption==='viewInfo'?styles.activeSubOption:null)}>View Info</li> */}
-                        {/* <li onClick={()=>{Navigator('/superAdminDashboard/studentActions/addCourses')}} className={styles.subOptions+' ' + (activeSubOption==='addCourses'?styles.activeSubOption:null)}>Add Courses</li> */}
-                        {/* </ul> */}
+                        <p onClick={()=>{changeSubMenu('complaint')}} className={(activeOption==='complaints'?styles.activeItem:null)+ ' flex items-center gap-2'}><SlSupport /> <span className={(state?null:styles.hidden)+' mt-1'}>Complaint</span></p>
+                        <ul className={state&&subComplaint?null:styles.hidden}>
+                        <li onClick={()=>{Navigator('/adminDashboard/complaints/complaints')}} className={styles.subOptions+' ' + (activeSubOption==='complaint'?styles.activeSubOption:null)}>View complaints</li>
+                        </ul>
                     
+            </div>
+
+            {/* application */}
+            <div  className={styles.item +' '+(state?styles.ItemOpenMenu:styles.ItemCloseMenu)}>
+                        <p onClick={()=>{changeSubMenu('application')}} className={(activeOption==='application'?styles.activeItem:null)+ ' flex items-center gap-2'}><FaRegFileAlt /> <span className={(state?null:styles.hidden)+' mt-1'}>Application</span></p>
+                        <ul className={state&&subApplication?null:styles.hidden}>
+                        <li onClick={()=>{navigator('/adminDashboard/admin/application')}} className={(state?null:styles.hidden)+' '+styles.subOptions+' ' + (activeSubOption==='application'?styles.activeSubOption:null)}>New Application</li>
+                        <li onClick={()=>{navigator('/adminDashboard/admin/applicationstatus')}} className={(state?null:styles.hidden)+' '+styles.subOptions+' ' + (activeSubOption==='applicationstatus'?styles.activeSubOption:null)}>Status of Application</li>
+                        </ul>
             </div>
 
             {/* Admin Guest Panel */}
@@ -235,6 +311,8 @@ export default function Sidebar(){
         </div>
 
  
+
+ {/* Small Screen */}
     </IconContext.Provider>
     <div className={`block w-full ${state?'h-full':'h-[60px]'} fixed transition-all top-0 left-0 backdrop-blur-md z-50 md:hidden`}>
             <div className='h-full w-full bg-[#131133] absolute top-0 left-0 opacity-85 -z-1'></div>
@@ -248,6 +326,8 @@ export default function Sidebar(){
                     <div id='ham3' className='w-[25px] h-[4px] bg-white rounded-sm mt-1 origin-center transition-all'></div>
                 </div>
             </div>
+
+
             <div className={`${state?'':'hidden'} absolute w-full h-full top-0 left-[0] flex justify-center items-center text-white`}>
                 <ul>
                     <li className='cursor-pointer flex flex-col text-xl font-semibold'><div onClick={()=>{changeSubMenu('Home')}} className={`flex items-center gap-2 ${activeOption==='main'?'text-orange-400':'text-white'}`}><IoHome size='15px'/> Main </div>
@@ -256,16 +336,55 @@ export default function Sidebar(){
                         </ul>
                     </li>
 
+                    {/* Student Info */}
+
                     <li className='mt-4 cursor-pointer flex flex-col text-xl font-semibold'><div onClick={()=>{changeSubMenu('studentInfo')}} className={`flex items-center gap-2 ${activeOption==='studentInfo'?'text-orange-400':'text-white'}`}><FaInfo size='15px' />  Student Info</div>
                         <ul className={`${subStudent?'':'hidden'} text-sm text-white  font-thin ml-8 transition-all`}>
                             <li onClick={()=>{Navigator('/adminDashboard/studentInfo/viewInfo');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='viewInfo'?'bg-blue-900 font-normal':''}`}> View Info</li>
                             <li onClick={()=>{Navigator('/adminDashboard/studentInfo/uploadInfo');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='uploadInfo'?'bg-blue-900 font-normal':''}`}> Upload Info</li>
+                            <li onClick={()=>{Navigator('/adminDashboard/studentInfo/register');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='register'?'bg-blue-900 font-normal':''}`}> Register Students</li>
+
                         </ul>
                     </li>
 
-                    <li className='mt-4 cursor-pointer flex flex-col text-xl font-semibold'><div onClick={()=>{changeSubMenu('roomInfo')}} className={`flex items-center gap-2 ${activeOption==='roomInfo'?'text-orange-400':'text-white'}`}><MdOutlineBedroomChild size="15px" /> Rooms</div>
+                    {/* Room Info */}
+                    <li className='mt-4 cursor-pointer flex flex-col text-xl font-semibold'><div onClick={()=>{changeSubMenu('roomInfo')}} className={`flex items-center gap-2 ${activeOption==='roomInfo'?'text-orange-400':'text-white'}`}><MdOutlineBedroomChild size="15px" /> Room Info</div>
                         <ul className={`${subRoom?'':'hidden'} text-sm text-white  font-thin ml-8 transition-all`}>
                             <li onClick={()=>{Navigator('/adminDashboard/roomInfo/allotRooms');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='allotRooms'?'bg-blue-900 font-normal':''}`}> Allot Rooms</li>
+                        </ul>
+                    </li>
+
+                    {/* Notice */}
+                    <li className='mt-4 cursor-pointer flex flex-col text-xl font-semibold'><div onClick={()=>{changeSubMenu('notice')}} className={`flex items-center gap-2 ${activeOption==='notice'?'text-orange-400':'text-white'}`}><FaNoteSticky size="15px" /> Notice</div>
+                        <ul className={`${subNotice?'':'hidden'} text-sm text-white  font-thin ml-8 transition-all`}>
+                            <li onClick={()=>{Navigator('/adminDashboard/notice/uploadNotice');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='uploadNotice'?'bg-blue-900 font-normal':''}`}>Upload Notice</li>
+                            <li onClick={()=>{Navigator('/adminDashboard/notice/viewNotice');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='viewNotice'?'bg-blue-900 font-normal':''}`}>View Notice</li>
+                        </ul>
+                    </li>
+
+                    {/* Complaints */}
+                    <li className='mt-4 cursor-pointer flex flex-col text-xl font-semibold'><div onClick={()=>{changeSubMenu('complaint')}} className={`flex items-center gap-2 ${activeOption==='complaint'?'text-orange-400':'text-white'}`}><SlSupport size="15px" /> Complaints</div>
+                        <ul className={`${subComplaint?'':'hidden'} text-sm text-white  font-thin ml-8 transition-all`}>
+                            <li onClick={()=>{Navigator('/adminDashboard/complaints/complaints');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='complaint'?'bg-blue-900 font-normal':''}`}>View Complaints</li>
+                        </ul>
+                    </li>
+
+                    {/* Applications */}
+                    <li className='mt-4 cursor-pointer flex flex-col text-xl font-semibold'><div onClick={()=>{changeSubMenu('application')}} className={`flex items-center gap-2 ${activeOption==='application'?'text-orange-400':'text-white'}`}><FaRegFileAlt size="15px" /> Applications</div>
+                        <ul className={`${subApplication?'':'hidden'} text-sm text-white  font-thin ml-8 transition-all`}>
+                            <li onClick={()=>{Navigator('/adminDashboard/admin/application');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='application'?'bg-blue-900 font-normal':''}`}>New Application</li>
+                            <li onClick={()=>{Navigator('/adminDashboard/admin/applicationstatus');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='applicationstatus'?'bg-blue-900 font-normal':''}`}>Status of Applications</li>
+                        </ul>
+                    </li>
+
+                    {/* Guest Info */}
+                    <li className='mt-4 cursor-pointer flex flex-col text-xl font-semibold'><div onClick={()=>{changeSubMenu('guest')}} className={`flex items-center gap-2 ${activeOption==='guest'?'text-orange-400':'text-white'}`}><MdLocalHotel size="15px" /> Guest</div>
+                        <ul className={`${subGuest?'':'hidden'} text-sm text-white  font-thin ml-8 transition-all`}>
+                            <li onClick={()=>{Navigator('/adminDashboard/guest/verify');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='verify'?'bg-blue-900 font-normal':''}`}>Verify Guest</li>
+                            <li onClick={()=>{Navigator('/adminDashboard/guest/viewSchedule');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='viewSchedule'?'bg-blue-900 font-normal':''}`}>View Guests Schedule</li>
+                            <li onClick={()=>{Navigator('/adminDashboard/guest/allot');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='allot'?'bg-blue-900 font-normal':''}`}>Allot Rooms to Guests</li>
+                            <li onClick={()=>{Navigator('/adminDashboard/guest/viewDetail');handleHamBurger()}} className={`text-xl hover:scale-110 transition-all rounded-md px-2 py-[2px] ${activeSubOption==='viewDetail'?'bg-blue-900 font-normal':''}`}>View Guests Details</li>
+                        
                         </ul>
                     </li>
                 </ul>
