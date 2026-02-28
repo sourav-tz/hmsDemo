460256254a82a89ab122938134ca438c8e2f6360
Soumik2704 <123760859+Xmisra@users.noreply.github.com>
2026-02-28 16:53:26 +0530

    Update addharNumber validation to 16 digits

--- backend/models/studentArchive.js
+++ backend/models/studentArchive.js
@@ -103,12 +103,13 @@ module.exports = (sequelize, DataTypes) => {
         dob: {
             type: DataTypes.DATEONLY,
         },
-        addharNumber: {
+         addharNumber: {
             type: DataTypes.STRING,
+            allowNull: true,
             validate: {
                 is: {
-                    args: /^[0-9]{12}$/i,
-                    msg: "addharNumber must be 12 digits",
+                    args: /^\d{16}$/,
+                    msg: "addharNumber must be exactly 16 digits (Virtual Aadhaar ID)",
                 },
             },
         },
